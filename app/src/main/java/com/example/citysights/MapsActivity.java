7ebd16cc9da89d8b457d4a027a82a227b3f999c1
Mohit <ageom140199@gmail.com>
2021-11-09 18:40:54 -0500

    Fourth Commit

--- app/src/main/java/com/example/citysights/MapsActivity.java
+++ app/src/main/java/com/example/citysights/MapsActivity.java
@@ -4,15 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMarkerDragListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         options.position(location);
         options.title("University of Windsor");
         options.snippet("This is University of Windsor");
+        options.draggable(true);
+
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 18f));
         googleMap.addMarker(options);
@@ -39,4 +43,26 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        Log.d("MapsActivity", marker.getTitle());
+        return true;
+    }
+
+    @Override
+    public void onMarkerDrag(@NonNull Marker marker) {
+        Log.d("MapsActivity", marker.getPosition().toString());
+    }
+
+    @Override
+    public void onMarkerDragStart(@NonNull Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker) {
+
+    }
+
 }
